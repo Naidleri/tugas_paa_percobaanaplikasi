30f09426f70790f6bf08cc4b5672cb94f5369a12
haris <haris@ONLS>
2024-03-08 19:12:08 +0700

    Add project files.

---
+++ PercobaanApi1/Controllers/PersonbyId.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PercobaanApi1.Models;
+
+namespace PercobaanApi1.Controllers
+{
+    public class PersonbyId : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("api/person/{id_person}")]
+        public ActionResult<Person> GetPersonbyId(int id_person)
+        {
+            PersonContext context = new PersonContext();
+            Person person = context.ListPerson().FirstOrDefault(p => p.id_person == id_person);
+            if (person != null)
+            {
+                return Ok(person);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
