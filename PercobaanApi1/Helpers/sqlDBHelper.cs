18a1a5e836e73ff1f3d4dce1b25db4d4a716068b
haris ardian <harisfirman387@gmail.com>
2024-05-17 17:46:03 +0700

    feat:helper and model

--- PercobaanApi1/Helpers/sqlDBHelper.cs
+++ PercobaanApi1/Helpers/sqlDBHelper.cs
@@ -1,4 +1,4 @@
-﻿using Npgsql;
+﻿ using Npgsql;
 using System.Data;
 
 namespace PercobaanApi1.Helpers
---
+++ PercobaanApi1/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿namespace PercobaanApi1.Models
+{
+    public class Login
+    {
+        public int id_person { get; set; }
+        public string nama { get; set; }
+        public string alamat { get; set; }
+        public string email { get; set; }
+        public int peran_fk { get; set; }
+        public string nama_peran { get; set; }
+        public string token { get; set; }
+
+    }
+}
